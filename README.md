d817076532f7afdb1d17639b7867c7734d36d1d7
Christiaan Westerbeek <chris@devotis.nl>
2018-12-08 15:13:13 +0100

    Concept

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # switchenv
 Command line tool to quickly switch .env file from Keepass entries (content from the Notes field)
+
+## Install
+
+`yarn add -D switchenv`
+
+or
+
+`npm install -D switchenv`
+
+## Usage
+
+In your app's `package.json` file add a switchenv command in the `scripts` section.
+
+```
+{
+  "name": "your-app",
+  "bin": {
+    "switchenv": "./index.js -d envs -f env.kdbx -k keyfile -e $1"
+  },
+   "dependencies": {
+    ...
+  },
+
+  "devDependencies": {
+    ...
+  }
+}
+```
---
+++ index.js
@@ -0,0 +1,58 @@
+const kdbxweb = require('kdbxweb')
+const fs = require('fs')
+const path = require('path')
+const util = require('util')
+const argv = require('minimist')(process.argv.slice(2))
+fs.readFileAsync = util.promisify(fs.readFile)
+fs.writeFileAsync = util.promisify(fs.writeFile)
+
+const {
+  d: keepassDir,
+  f: keepassFile,
+  k: keepassKeyfile,
+  e: keepassEntryTitle
+} = argv;
+
+const read = async () => {
+  const keyFileArrayBuffer = await fs.readFileAsync(path.join(__dirname, keepassDir, keepassKeyfile));
+  const dataAsArrayBuffer = await fs.readFileAsync(path.join(__dirname, keepassDir, keepassFile));
+
+  const credentials = new kdbxweb.Credentials(
+    kdbxweb.ProtectedValue.fromString(''), // keyfile only kdbx file
+    keyFileArrayBuffer.buffer
+  );
+
+  const db = await kdbxweb.Kdbx.load(dataAsArrayBuffer.buffer, credentials)
+
+  const entries = db.getDefaultGroup().entries
+
+  return entries
+}
+
+read()
+  .then((entries) => {
+    const filteredEntries = entries
+      .filter(entry => entry.fields && entry.fields.Title === keepassEntryTitle)
+      .map(entry => entry.fields.Notes )
+
+    if (filteredEntries.length !== 1) {
+      throw new Error(`Exactly 1 entry expected, but ${filteredEntries.length} are found by title ${keepassEntryTitle}`);
+    }
+
+    const [content] = filteredEntries;
+
+    console.log(`Writing the Notes of ${keepassEntryTitle} to .env ...`)
+    return content;
+  })
+  .then(content => {
+    return fs.writeFileAsync(
+      path.join(__dirname, '../.env'),
+      content
+    )
+  })
+  .then(() => {
+    console.log('Done')
+  })
+  .catch((result) => {
+    console.log('err', result)
+  })
